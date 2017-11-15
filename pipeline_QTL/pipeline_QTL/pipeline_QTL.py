27ba7eb2841c0f310e0849a7e0ac6c9ce10ca730
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-11-15 18:27:02 +0000

    adding getDir to top of directory

--- pipeline_QTL/pipeline_QTL/pipeline_QTL.py
+++ pipeline_QTL/pipeline_QTL/pipeline_QTL.py
@@ -188,25 +188,6 @@ from builtins import dict
 ################
 
 ################
-# Get locations of source code (this file)
-    # os.path.join note: a subsequent argument with an '/' discards anything
-    # before it
-    # For function to search path see: 
-    # http://stackoverflow.com/questions/4519127/setuptools-package-data-folder-location
-# MANIFEST.in file instructs the project_quickstart/templates folder to be included in installation
-
-_ROOT = os.path.abspath(os.path.dirname(__file__))
-def getDir(path = _ROOT):
-    ''' Get the absolute path to where this function resides. Useful for
-    determining the user's path to a package. If a sub-directory is given it
-    will be added to the path returned. Use '..' to go up directory levels. '''
-   # src_top_dir = os.path.abspath(os.path.join(_ROOT, '..'))
-    src_dir = _ROOT
-    return(os.path.abspath(os.path.join(src_dir, path)))
-
-################
-
-################
 # Load options from the config file
 # Pipeline configuration
 ini_paths = [os.path.abspath(os.path.dirname(sys.argv[0])),
